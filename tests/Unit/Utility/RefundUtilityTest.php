06ade710241aa6e1c10b2f481ae5142c36ba7692
tomas862 <tomas862@users.noreply.github.com>
2020-11-27 13:26:23 +0000

    Apply php-cs-fixer changes

--- tests/Unit/Utility/RefundUtilityTest.php
+++ tests/Unit/Utility/RefundUtilityTest.php
@@ -7,253 +7,252 @@ use PHPUnit\Framework\TestCase;
 
 class RefundUtilityTest extends TestCase
 {
-    /**
-     * @dataProvider getRefundLinesDataProvider
-     *
-     * @param $lines
-     * @param $result
-     */
-    public function testGetRefundLines($lines, $result)
-    {
-        $refunds = RefundUtility::getRefundLines($lines);
+	/**
+	 * @dataProvider getRefundLinesDataProvider
+	 *
+	 * @param $lines
+	 * @param $result
+	 */
+	public function testGetRefundLines($lines, $result)
+	{
+		$refunds = RefundUtility::getRefundLines($lines);
 
-        self::assertEquals($result, $refunds);
-    }
+		self::assertEquals($result, $refunds);
+	}
 
-    /**
-     * @dataProvider getIsOrderLinesRefundPossibleDataProvider
-     *
-     * @param $lines
-     * @param $availableRefund
-     * @param $result
-     */
-    public function testIsOrderLinesRefundPossible($lines, $availableRefund, $result)
-    {
-        $refunds = RefundUtility::isOrderLinesRefundPossible($lines, $availableRefund);
+	/**
+	 * @dataProvider getIsOrderLinesRefundPossibleDataProvider
+	 *
+	 * @param $lines
+	 * @param $availableRefund
+	 * @param $result
+	 */
+	public function testIsOrderLinesRefundPossible($lines, $availableRefund, $result)
+	{
+		$refunds = RefundUtility::isOrderLinesRefundPossible($lines, $availableRefund);
 
-        self::assertEquals($result, $refunds);
-    }
+		self::assertEquals($result, $refunds);
+	}
 
-    public function getRefundLinesDataProvider()
-    {
-        return [
-            'normal refund' => [
-                'lines' => [
-                    0 => [
-                        'id' => 'odl_tnw1ay',
-                        'orderId' => 'ord_7v67h8',
-                        'quantity' => 1,
-                        'totalAmount' => [
-                            'value' => '100.00',
-                            'currency' => 'EUR'
-                        ],
-                    ]
-                ],
-                'result' => [
-                    'lines' => [
-                        0 => [
-                            'id' => 'odl_tnw1ay',
-                            'quantity' => 1,
-                            'amount' => [
-                                'value' => '100.00',
-                                'currency' => 'EUR'
-                            ]
-                        ]
-                    ]
-                ]
-            ],
-            'voucher refund' => [
-                'lines' => [
-                    0 => [
-                        'id' => 'odl_tnw1ay',
-                        'orderId' => 'ord_7v67h8',
-                        'quantity' => 1,
-                        'totalAmount' => [
-                            'value' => '100.00',
-                            'currency' => 'EUR'
-                        ],
-                        'amountRefunded' => [
-                            'value' => '0.00',
-                            'currency' => 'EUR'
-                        ]
-                    ]
-                ],
-                'result' => [
-                    'lines' => [
-                        0 => [
-                            'id' => 'odl_tnw1ay',
-                            'quantity' => 1,
-                            'amount' => [
-                                'value' => '100.00',
-                                'currency' => 'EUR'
-                            ]
-                        ]
-                    ]
-                ]
-            ],
-            'voucher refund 2 products' => [
-                'lines' => [
-                    0 => [
-                        'id' => 'odl_tnw1ay',
-                        'orderId' => 'ord_7v67h8',
-                        'quantity' => 1,
-                        'totalAmount' => [
-                            'value' => '100.00',
-                            'currency' => 'EUR'
-                        ],
-                    ],
-                    1 => [
-                        'id' => 'odl_tnw1aa',
-                        'orderId' => 'ord_7v67h8',
-                        'quantity' => 1,
-                        'totalAmount' => [
-                            'value' => '11.00',
-                            'currency' => 'EUR'
-                        ],
-                    ]
-                ],
-                'result' => [
-                    'lines' => [
-                        0 => [
-                            'id' => 'odl_tnw1ay',
-                            'quantity' => 1,
-                            'amount' => [
-                                'value' => '100.00',
-                                'currency' => 'EUR'
-                            ]
-                        ],
-                        1 => [
-                            'id' => 'odl_tnw1aa',
-                            'quantity' => 1,
-                            'amount' => [
-                                'value' => '11.00',
-                                'currency' => 'EUR'
-                            ]
-                        ]
-                    ]
-                ]
-            ],
-        ];
-    }
+	public function getRefundLinesDataProvider()
+	{
+		return [
+			'normal refund' => [
+				'lines' => [
+					0 => [
+						'id' => 'odl_tnw1ay',
+						'orderId' => 'ord_7v67h8',
+						'quantity' => 1,
+						'totalAmount' => [
+							'value' => '100.00',
+							'currency' => 'EUR',
+						],
+					],
+				],
+				'result' => [
+					'lines' => [
+						0 => [
+							'id' => 'odl_tnw1ay',
+							'quantity' => 1,
+							'amount' => [
+								'value' => '100.00',
+								'currency' => 'EUR',
+							],
+						],
+					],
+				],
+			],
+			'voucher refund' => [
+				'lines' => [
+					0 => [
+						'id' => 'odl_tnw1ay',
+						'orderId' => 'ord_7v67h8',
+						'quantity' => 1,
+						'totalAmount' => [
+							'value' => '100.00',
+							'currency' => 'EUR',
+						],
+						'amountRefunded' => [
+							'value' => '0.00',
+							'currency' => 'EUR',
+						],
+					],
+				],
+				'result' => [
+					'lines' => [
+						0 => [
+							'id' => 'odl_tnw1ay',
+							'quantity' => 1,
+							'amount' => [
+								'value' => '100.00',
+								'currency' => 'EUR',
+							],
+						],
+					],
+				],
+			],
+			'voucher refund 2 products' => [
+				'lines' => [
+					0 => [
+						'id' => 'odl_tnw1ay',
+						'orderId' => 'ord_7v67h8',
+						'quantity' => 1,
+						'totalAmount' => [
+							'value' => '100.00',
+							'currency' => 'EUR',
+						],
+					],
+					1 => [
+						'id' => 'odl_tnw1aa',
+						'orderId' => 'ord_7v67h8',
+						'quantity' => 1,
+						'totalAmount' => [
+							'value' => '11.00',
+							'currency' => 'EUR',
+						],
+					],
+				],
+				'result' => [
+					'lines' => [
+						0 => [
+							'id' => 'odl_tnw1ay',
+							'quantity' => 1,
+							'amount' => [
+								'value' => '100.00',
+								'currency' => 'EUR',
+							],
+						],
+						1 => [
+							'id' => 'odl_tnw1aa',
+							'quantity' => 1,
+							'amount' => [
+								'value' => '11.00',
+								'currency' => 'EUR',
+							],
+						],
+					],
+				],
+			],
+		];
+	}
 
-    public function getIsOrderLinesRefundPossibleDataProvider()
-    {
-        return [
-            'normal refund' => [
-                'lines' => [
-                    0 => [
-                        'id' => 'odl_tnw1ay',
-                        'orderId' => 'ord_7v67h8',
-                        'quantity' => 1,
-                        'totalAmount' => [
-                            'value' => '100.00',
-                            'currency' => 'EUR'
-                        ],
-                        'amountRefunded' => [
-                            'value' => '0.00',
-                            'currency' => 'EUR'
-                        ]
-                    ]
-                ],
-                'remainingAmount' => [
-                    'value' => '100.00',
-                    'currency' => 'EUR'
-                ],
-                'result' => true
-            ],
-            'voucher refund' => [
-                'lines' => [
-                    0 => [
-                        'id' => 'odl_tnw1ay',
-                        'orderId' => 'ord_7v67h8',
-                        'quantity' => 1,
-                        'totalAmount' => [
-                            'value' => '100.00',
-                            'currency' => 'EUR'
-                        ],
-                        'amountRefunded' => [
-                            'value' => '0.00',
-                            'currency' => 'EUR'
-                        ]
-                    ]
-                ],
-                'remainingAmount' => [
-                    'value' => '90.00',
-                    'currency' => 'EUR'
-                ],
-                'result' => false
-            ],
-            'voucher refund 2 products' => [
-                'lines' => [
-                    0 => [
-                        'id' => 'odl_tnw1ay',
-                        'orderId' => 'ord_7v67h8',
-                        'quantity' => 1,
-                        'totalAmount' => [
-                            'value' => '100.00',
-                            'currency' => 'EUR'
-                        ],
-                        'amountRefunded' => [
-                            'value' => '0.00',
-                            'currency' => 'EUR'
-                        ]
-                    ],
-                    1 => [
-                        'id' => 'odl_tnw1aa',
-                        'orderId' => 'ord_7v67h8',
-                        'quantity' => 1,
-                        'totalAmount' => [
-                            'value' => '11.00',
-                            'currency' => 'EUR'
-                        ],
-                        'amountRefunded' => [
-                            'value' => '0.00',
-                            'currency' => 'EUR'
-                        ]
-                    ]
-                ],
-                'remainingAmount' => [
-                    'value' => '99.00',
-                    'currency' => 'EUR'
-                ],
-                'result' => false
-            ],
-            'voucher refund 2 products with quantity' => [
-                'lines' => [
-                    0 => [
-                        'id' => 'odl_tnw1ay',
-                        'orderId' => 'ord_7v67h8',
-                        'quantity' => 5,
-                        'totalAmount' => [
-                            'value' => '500.00',
-                            'currency' => 'EUR'
-                        ],
-                        'amountRefunded' => [
-                            'value' => '0.00',
-                            'currency' => 'EUR'
-                        ]
-                    ],
-                    1 => [
-                        'id' => 'odl_tnw1aa',
-                        'orderId' => 'ord_7v67h8',
-                        'quantity' => 3,
-                        'totalAmount' => [
-                            'value' => '33.00',
-                            'currency' => 'EUR'
-                        ],
-                        'amountRefunded' => [
-                            'value' => '0.00',
-                            'currency' => 'EUR'
-                        ]
-                    ]
-                ],
-                'remainingAmount' => [
-                    'value' => '500.00',
-                    'currency' => 'EUR'
-                ],
-                'result' => false
-            ]
-        ];
-    }
-
+	public function getIsOrderLinesRefundPossibleDataProvider()
+	{
+		return [
+			'normal refund' => [
+				'lines' => [
+					0 => [
+						'id' => 'odl_tnw1ay',
+						'orderId' => 'ord_7v67h8',
+						'quantity' => 1,
+						'totalAmount' => [
+							'value' => '100.00',
+							'currency' => 'EUR',
+						],
+						'amountRefunded' => [
+							'value' => '0.00',
+							'currency' => 'EUR',
+						],
+					],
+				],
+				'remainingAmount' => [
+					'value' => '100.00',
+					'currency' => 'EUR',
+				],
+				'result' => true,
+			],
+			'voucher refund' => [
+				'lines' => [
+					0 => [
+						'id' => 'odl_tnw1ay',
+						'orderId' => 'ord_7v67h8',
+						'quantity' => 1,
+						'totalAmount' => [
+							'value' => '100.00',
+							'currency' => 'EUR',
+						],
+						'amountRefunded' => [
+							'value' => '0.00',
+							'currency' => 'EUR',
+						],
+					],
+				],
+				'remainingAmount' => [
+					'value' => '90.00',
+					'currency' => 'EUR',
+				],
+				'result' => false,
+			],
+			'voucher refund 2 products' => [
+				'lines' => [
+					0 => [
+						'id' => 'odl_tnw1ay',
+						'orderId' => 'ord_7v67h8',
+						'quantity' => 1,
+						'totalAmount' => [
+							'value' => '100.00',
+							'currency' => 'EUR',
+						],
+						'amountRefunded' => [
+							'value' => '0.00',
+							'currency' => 'EUR',
+						],
+					],
+					1 => [
+						'id' => 'odl_tnw1aa',
+						'orderId' => 'ord_7v67h8',
+						'quantity' => 1,
+						'totalAmount' => [
+							'value' => '11.00',
+							'currency' => 'EUR',
+						],
+						'amountRefunded' => [
+							'value' => '0.00',
+							'currency' => 'EUR',
+						],
+					],
+				],
+				'remainingAmount' => [
+					'value' => '99.00',
+					'currency' => 'EUR',
+				],
+				'result' => false,
+			],
+			'voucher refund 2 products with quantity' => [
+				'lines' => [
+					0 => [
+						'id' => 'odl_tnw1ay',
+						'orderId' => 'ord_7v67h8',
+						'quantity' => 5,
+						'totalAmount' => [
+							'value' => '500.00',
+							'currency' => 'EUR',
+						],
+						'amountRefunded' => [
+							'value' => '0.00',
+							'currency' => 'EUR',
+						],
+					],
+					1 => [
+						'id' => 'odl_tnw1aa',
+						'orderId' => 'ord_7v67h8',
+						'quantity' => 3,
+						'totalAmount' => [
+							'value' => '33.00',
+							'currency' => 'EUR',
+						],
+						'amountRefunded' => [
+							'value' => '0.00',
+							'currency' => 'EUR',
+						],
+					],
+				],
+				'remainingAmount' => [
+					'value' => '500.00',
+					'currency' => 'EUR',
+				],
+				'result' => false,
+			],
+		];
+	}
 }
