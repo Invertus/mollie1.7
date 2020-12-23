03c43e5d9dd8bef9ca3ed259b0cda9fbcba665b8
Tomas Ilginis <tilginis@gmail.com>
2020-12-23 15:19:06 +0200

    changes image

--- tests/seed/settings/parameters.php
+++ tests/seed/settings/parameters.php
@@ -1,26 +1,25 @@
-<?php
-
-return [
-  'parameters' => [
-	'database_host' => 'mysql',
-	'database_port' => '',
-	'database_name' => 'prestashop',
-	'database_user' => 'root',
-	'database_password' => 'prestashop',
-	'database_prefix' => 'ps_',
-	'database_engine' => 'InnoDB',
-	'mailer_transport' => 'smtp',
-	'mailer_host' => '127.0.0.1',
-	'mailer_user' => null,
-	'mailer_password' => null,
-	'secret' => '9bpAcdHeBCD9YdLsavpDFHy5uR2djJEIS1XXpE5yJ1hdSRs6waAzIqfk',
-	'ps_caching' => 'CacheMemcache',
-	'ps_cache_enable' => false,
-	'ps_creation_date' => '2020-12-04',
-	'locale' => 'en-US',
-	'use_debug_toolbar' => true,
-	'cookie_key' => '6NiLzzVp8Z9LiOtHgBNScv929I2K8a2A8jNppJVz1t5WjnnRGPbXVXKS',
-	'cookie_iv' => 'TtPM0O7S',
-	'new_cookie_key' => 'def000005380afacc8d6f1b3a5fb701144611a633beb5f5c39d970737ec1b2d99d4b76bdc9fe462000e1868662f45cebef09d85d3979e5795e16a9d3e4276144af4a1f54',
-  ],
-];
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'mysql',
+    'database_port' => '',
+    'database_name' => 'prestashop',
+    'database_user' => 'root',
+    'database_password' => 'prestashop',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => '5me4mdumTjYo4jPpNtQSuRN1MyD9wSSze7VuLg4s2ktMUxkXHihrLXZV1NaHuzhU',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2020-12-23',
+    'locale' => 'en-US',
+    'use_debug_toolbar' => true,
+    'cookie_key' => '6NiLzzVp8Z9LiOtHgBNScv929I2K8a2A8jNppJVz1t5WjnnRGPbXVXKS',
+    'cookie_iv' => 'TtPM0O7S',
+    'new_cookie_key' => 'def000001b90465d16f2aa760abe76e1a88fb80071075978db96754d16fd83cef2c9d8d1218a6dba2f28cee48e527700e85ddf87731652776449d60f9f4e1cceb90198b2',
+  ),
+);
