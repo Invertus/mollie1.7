3ba1cc9fe819c4db517b1ab90ad5e3d889fbb94d
SimonasB88 <simonas.burneikis@invertus.eu>
2022-08-31 18:32:56 +0300

    Update index.js

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -37,6 +37,7 @@ import 'cypress-iframe';
 require('cypress-iframe');
 // Import commands.js using ES2015 syntax:
 import './commands'
+require('cypress-commands');
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
@@ -49,39 +50,3 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 Cypress.Keyboard.defaults({
   keystrokeDelay: 0,
 })
-// Cypress.on('window:before:load', (win) => {
-//         Cypress.log({
-//             name: 'console.log',
-//             message: 'wrap on console.log',
-//         });
-
-//         // pass through cypress log so we can see log inside command execution order
-//         win.console.log = (...args) => {
-//             Cypress.log({
-//                 name: 'console.log',
-//                 message: args,
-//             });
-//         };
-//     });
-
-//     Cypress.on('log:added', (options) => {
-//         if (options.instrument === 'command') {
-//             // eslint-disable-next-line no-console
-//             console.log(
-//                 `${(options.displayName || options.name || '').toUpperCase()} ${
-//                     options.message
-//                 }`,
-//             );
-//         }
-//     });
-// Cypress.on('window:before:load', (win) => {
-//   cy.spy(win.console, 'error');
-//   cy.spy(win.console, 'warn');
-// });
-
-// afterEach(() => {
-//   cy.window().then((win) => {
-//     expect(win.console.error).to.have.callCount(0);
-//     expect(win.console.warn).to.have.callCount(0);
-//   });
-// });
